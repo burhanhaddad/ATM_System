008fc84dfe3643a8dc3533d4cfc44ce153ed7c64
Burhan Haddad <burhanhaddad5@gmail.com>
2026-05-13 14:03:24 +0300

    Added login functionality and file reading

--- ATM_System/ATM_System.cpp
+++ ATM_System/ATM_System.cpp
@@ -1,20 +1,161 @@
-// ATM_System.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iomanip>
+using namespace std;
+
+const string ClientsFileName = "Clients.txt";
+
+struct sClient
+{
+    string AccountNumber;
+    string PinCode;
+    string Name;
+    string Phone;
+    double AccountBalance;
+    bool MarkForDelete = false;
+
+
+};
+sClient CurrentClient;
+vector<string> SplitString(string S1, string Delim)
+{
+
+    vector<string> vString;
+
+    short pos = 0;
+    string sWord; // define a string variable  
+
+    // use find() function to get the position of the delimiters  
+    while ((pos = S1.find(Delim)) != std::string::npos)
+    {
+        sWord = S1.substr(0, pos); // store the word   
+        if (sWord != "")
+        {
+            vString.push_back(sWord);
+        }
+
+        S1.erase(0, pos + Delim.length());  /* erase() until positon and move to next word. */
+    }
+
+    if (S1 != "")
+    {
+        vString.push_back(S1); // it adds last word of the string.
+    }
+
+    return vString;
+
+}
+
+sClient ConvertLinetoRecord(string Line, string Seperator = "#//#")
+{
+
+    sClient Client;
+    vector<string> vClientData;
+
+    vClientData = SplitString(Line, Seperator);
+
+    Client.AccountNumber = vClientData[0];
+    Client.PinCode = vClientData[1];
+    Client.Name = vClientData[2];
+    Client.Phone = vClientData[3];
+    Client.AccountBalance = stod(vClientData[4]);//cast string to double
+
+
+    return Client;
+
+}
+
+vector <sClient> LoadCleintsDataFromFile(string FileName)
+{
+
+    vector <sClient> vClients;
+
+    fstream MyFile;
+    MyFile.open(FileName, ios::in);
+
+    if (MyFile.is_open())
+    {
+
+        string Line;
+        sClient Client;
+
+        while (getline(MyFile, Line))
+        {
+
+            Client = ConvertLinetoRecord(Line);
+
+            vClients.push_back(Client);
+        }
+
+        MyFile.close();
+
+    }
+
+    return vClients;
+
+}
+
+bool FindClientByAccountNumber(string AccountNumber,string PinCode, sClient& Client)
+{
+	vector <sClient> vClients = LoadCleintsDataFromFile(ClientsFileName);
+
+    for (sClient C : vClients)
+    {
+
+        if (C.AccountNumber == AccountNumber)
+        {
+            Client = C;
+            return true;
+        }
+
+    }
+    return false;
+
+}
+
+bool LoadClientInfo(string AccountNumber, string PinCode) {
+
+    if (FindClientByAccountNumber(AccountNumber, PinCode, CurrentClient))
+    {
+        return true;
+
+    }
+    else
+        return false;
+}
+
+void Login() {
+
+    string AccountNumber, PinCode;
+    bool LoginFaild = false;
+    do
+    {
+        system("cls");
+        cout << "\n---------------------------------\n";
+        cout << "\tLogin Screen";
+        cout << "\n---------------------------------\n";
+        if (LoginFaild)
+        {
+            cout << "Invalid AccountNumber/PinCode!\n";
+        }
+        cout << "Enter Account Number? ";
+        cin >> AccountNumber;
+        cout << "Enter Pin Code? ";
+        cin >> PinCode;
+        LoginFaild = !LoadClientInfo(AccountNumber, PinCode);
+    } while (LoginFaild);
+    
+
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Login();
+    
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
